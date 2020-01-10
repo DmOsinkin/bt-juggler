01d195204cd885488250dff92866938a06afec3a
Dmitry Osinkin <dm.osinkin@gmail.com>
2020-01-11 01:16:12 +0300

    Change main functional of application
    Now application trying to scan all BLE devices

--- app/src/main/java/ru/osinkin/bt_juggler/MainActivity.kt
+++ app/src/main/java/ru/osinkin/bt_juggler/MainActivity.kt
@@ -1,25 +1,87 @@
 package ru.osinkin.bt_juggler
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.*
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.osinkin.bt_juggler.adapters.PairedDevicesRecycleViewAdapter
-import ru.osinkin.bt_juggler.data.PairedDevicesController
 
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * private properties
+     */
+    private val deviceList = mutableListOf<BluetoothDevice>()
+    private lateinit var mBluetoothAdapter: BluetoothAdapter
+    private lateinit var mScanCallback: ScanCallback
+    private lateinit var mAdapter: PairedDevicesRecycleViewAdapter
+    private lateinit var mLeScanner: BluetoothLeScanner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        pairedDevicesRecyclerView.setHasFixedSize(true)
+
         val layoutManager = LinearLayoutManager(this)
         pairedDevicesRecyclerView.layoutManager = layoutManager
+        pairedDevicesRecyclerView.setHasFixedSize(true)
 
-        val mAdapter = PairedDevicesRecycleViewAdapter(PairedDevicesController.getPairedDevicesController(this).getPairedDevices())
-        pairedDevicesRecyclerView.adapter = mAdapter
+        prepareBLE()
     }
 
+    override fun onResume() {
+        super.onResume()
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (!mBluetoothAdapter.isEnabled) {
+            val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT)
+        } else {
+            startScan()
+            mAdapter =
+                PairedDevicesRecycleViewAdapter(deviceList)
+            pairedDevicesRecyclerView.adapter = mAdapter
+        }
+    }
 
+    private fun prepareBLE() {
+        // Initializes Bluetooth adapter.
+        val bluetoothManager: BluetoothManager =
+            getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        mBluetoothAdapter = bluetoothManager.adapter
+        initScanCallback()
+    }
+
+    private fun initScanCallback() {
+        mScanCallback = object : ScanCallback() {
+            override fun onScanResult(callbackType: Int, result: ScanResult) {
+                super.onScanResult(callbackType, result)
+                deviceList.add(result.device)
+            }
+        }
+
+    }
+
+    /**
+     * Initiates the scan for BLE devices according to the API level.
+     */
+    private fun startScan() {
+        mLeScanner = mBluetoothAdapter.bluetoothLeScanner
+        // start scan in low latency mode
+        mLeScanner.startScan(
+            ArrayList<ScanFilter>(),
+            ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build(),
+            mScanCallback
+        )
+    }
+
+    companion object {
+        const val REQUEST_ENABLE_BT = 1
+    }
 }
--- app/src/main/java/ru/osinkin/bt_juggler/adapters/PairedDevicesRecycleViewAdapter.kt
+++ app/src/main/java/ru/osinkin/bt_juggler/adapters/PairedDevicesRecycleViewAdapter.kt
@@ -1,5 +1,6 @@
 package ru.osinkin.bt_juggler.adapters
 
+import android.bluetooth.BluetoothDevice
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,14 +8,17 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ru.osinkin.bt_juggler.R
 
-class PairedDevicesRecycleViewAdapter(private val myDataset: MutableList<String>) : RecyclerView.Adapter<PairedDevicesRecycleViewAdapter.MyViewHolder>() {
+class PairedDevicesRecycleViewAdapter(private val myDataset: MutableList<BluetoothDevice>) :
+    RecyclerView.Adapter<PairedDevicesRecycleViewAdapter.MyViewHolder>() {
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    class MyViewHolder(pairedDevicesItemView: View) : RecyclerView.ViewHolder(pairedDevicesItemView) {
+    class MyViewHolder(pairedDevicesItemView: View) :
+        RecyclerView.ViewHolder(pairedDevicesItemView) {
         var textView: TextView? = null
+
         init {
             textView = pairedDevicesItemView.findViewById(R.id.paired_Devices_List_Item)
         }
@@ -23,7 +27,8 @@ class PairedDevicesRecycleViewAdapter(private val myDataset: MutableList<String>
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         // create a new view
-        val textView = LayoutInflater.from(parent.context).inflate(R.layout.paired_device_text_view, parent, false)
+        val textView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.paired_device_text_view, parent, false)
         return MyViewHolder(textView)
     }
 
@@ -31,7 +36,7 @@ class PairedDevicesRecycleViewAdapter(private val myDataset: MutableList<String>
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView?.text = if (myDataset.isNotEmpty()) myDataset[position] else "none"
+        holder.textView?.text = if (myDataset.isNotEmpty()) myDataset[position].name else "none"
     }
 
     // Return the size of your dataset (invoked by the layout manager)
